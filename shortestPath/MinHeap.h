db743a8a3e6515fba8ad375849b31c51e9f88031
DeadDude-glitch <ahmed.ussef248@gmail.com>
2022-05-30 13:29:52 +0200

    array size vs data size
    added function that return the size of the array and another that return the data size

--- shortestPath/MinHeap.h
+++ shortestPath/MinHeap.h
@@ -24,7 +24,7 @@ class MinHeap
 		Node extractMin	  ();							// returns & removes the node with minimum cost
 		void buildMinHeap ( Node[], int size );			// allocates array then builds a min-heap from an array of struct Node with the given size
 		void minHeapify ( int index, int size );		// restores the min-heap property for the “heap” array using the given index and size	
-		void decreaseKey  ( char label, int newCost );	// decreases the node that has the given label to newCost
+		void decreaseKey ( char label, int newCost );	// decreases the node that has the given label to newCost
 		int	 parent	( int i );							// returns the index of the parent of i by index
 		bool inHeap ( char c );							// checks if the node with the given label is in the heap
 		int	 getSize ();								// returns size of the heap (just a getter)
@@ -34,6 +34,8 @@ class MinHeap
 		int  right_child (int i);
 		void minHeapify ();								// minHeapify the entire tree
 		int  parent (char c);							// returns the index of the parent of c by label
+		bool isEmpty ();								// checks if heap is empty
+		int getAllocationSize();						// get the allocation size
 };
 
 
@@ -42,20 +44,32 @@ class MinHeap
 ////////////////////////////
 // none graded functios
 //////////////////////////
-int  MinHeap::left_child (int i) { return i * 2 + 1; }
-int  MinHeap::right_child(int i) { return i * 2 + 2; }
+int  MinHeap::left_child (int i) { return i * 2 + 1; }			// Left Child Index  = nodeIndex * 2 + 1
+int  MinHeap::right_child(int i) { return i * 2 + 2; }			// Right Child Index  = nodeIndex * 2 + 2
+bool MinHeap::isEmpty() { return this->_size == 0; }
+void MinHeap::minHeapify() { minHeapify(0, _size); }
+// returns array size of the heap
+int MinHeap::getAllocationSize()
+{	
+	// pointer calculation to find array size
+	// heap points to the first element									heap --> [1, 2, 3][ ,  ,  ]
+	// *(&heap+1) points to {_size} elements ahead of heap						 ^		  ^
+	// (heap first byte - heap last byte) / datatype byte size = size			heap     *(&heap+1)
+	return *(&heap + 1) - heap;
+	
+}
 
 
 // returns & removes the node with minimum cost
 Node MinHeap::extractMin()
 {	
-	/* 
-	* save root node into variable
-	* seet root node cost to last node cost
-	* delete last node
-	* minheapify( 0, _size - 1)
-	* return 
-	*/
+	// save root node into variable
+	Node root = heap[0];
+	// set root node to last node 
+	heap[0] = heap[_size];
+	// TODO: delete last node
+	minHeapify(0, _size--);									// and SEGMENTATION ERROR might occure here
+	return root;
 }
 
 // allocates array then builds a min-heap from an array of struct Node with the given size
@@ -84,7 +98,7 @@ void MinHeap::decreaseKey (char label,int newCost)
 int MinHeap::parent (int  i) { return (i -  1) / 2; }
 int MinHeap::parent (char c) { return (c - 98) / 2; }	// by label
 
-// returns size of the heap
+// returns current size of the heap
 int MinHeap::getSize()
 {	// pointer calculation to find array size
 	// heap points to the first element									heap --> [1, 2, 3][ ,  ,  ]
@@ -116,6 +130,6 @@ Heap Structure
 	--------------------------------------------------------------------------------------------------------------------------->
 
 	Parent Node Index  = (nodeIndex - 1) /2			[		 0         ]
-	Left   Node Index  = nodeIndex * 2 + 1 			[	1	 ][   2    ]
-	Right  Node Index  = nodeIndex * 2 + 2 		    [ 3 ][ 4 ][ 5 ][ 6 ]
+	Left   Child Index  = nodeIndex * 2 + 1 			[	1	 ][   2    ]
+	Right  Child Index  = nodeIndex * 2 + 2 		    [ 3 ][ 4 ][ 5 ][ 6 ]
 */
